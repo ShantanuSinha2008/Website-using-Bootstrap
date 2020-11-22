6cc0b08a94d3fa07f99dd0c19616b5659a9382dd
ShantanuSinha2008 <74371183+ShantanuSinha2008@users.noreply.github.com>
2020-11-22 13:22:09 +0530

    Add files via upload

---
+++ JavaScript/client.js
@@ -0,0 +1,8 @@
+const { Socket } = require('socket.io');
+const socket = io('http://localhost:8000');
+
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('messageInp')
+const messageContainer = document.querySelector(".container")
+
+const name = prompt("Enter your Name to join");
